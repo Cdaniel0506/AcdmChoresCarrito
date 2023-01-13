0a242af7bc3f237d7e3c1da36b3f611a1e6f9a20
Carlos Daniel <carlos2000.cdb@gmail.com>
2023-01-13 13:06:41 -0500

    Se agraga el proyecto carrito

---
+++ assets/js/data/data.js
@@ -0,0 +1,86 @@
+const db = [
+  {
+    id: 1,
+    nombre: 'La BiBlia',
+    descripcion: 'Conjunto de Escrituras Sagradas formado por el Antiguo y el Nuevo Testamento  ',
+    precio: 580000.00,
+    imagen: './assets/img/libro-1.png',
+    categoria: 'aperitivos',
+    cantidad: 10
+  },
+  {
+    id: 2,
+    nombre: 'Harry Potter - J. K. Rowling',
+    descripcion: 'Harry Potter es una serie de novelas fantásticas',
+    precio: 350000.00,
+    imagen: './assets/img/libro-2.png',
+    categoria: 'aperitivos',
+    cantidad: 15
+  },
+  {
+    id: 3,
+    nombre: 'El Principito - Antoine de Saint-E',
+    descripcion: 'El principito narra la historia de un piloto que, ...',
+    precio: 90000.00,
+    imagen: './assets/img/libro-3.png',
+    categoria: 'aperitivos',
+    cantidad: 13
+  },
+  {
+    id: 4,
+    nombre: 'El Alquimista Paulo Coelho',
+    descripcion: 'El alquimista es un libro escrito por el escritor brasileño Paulo Coelho...',
+    precio: 15000.00,
+    imagen: './assets/img/libro-4.png',
+    categoria: 'aperitivos',
+    cantidad: 16
+  },
+  {
+    id: 5,
+    nombre: 'One Piece - Eiichiro Oda',
+    descripcion: 'One Piece narra la historia de un joven llamado Monkey D. Luffy...',
+    precio: 256000.00,
+    imagen: './assets/img/libro-1.png',
+    categoria: 'aperitivos',
+    cantidad: 24
+  },
+  {
+    id: 6,
+    nombre: 'Los Vengadores',
+    descripcion: 'Los Vengadores fueron un equipo compuesto por personas extraordinarias...',
+    precio: 85000.00,
+    imagen: './assets/img/libro-2.png',
+    categoria: 'aperitivos',
+    cantidad: 20
+  },
+  {
+    id: 7,
+    nombre: 'aCien años de soledad -de Gabriel García',
+    descripcion: 'Muchos años después, frente al pelotón de fusilamiento, el coronel...',
+    precio: 369000.00,
+    imagen: './assets/img/libro-3.png',
+    categoria: 'bebidas',
+    cantidad: 14
+  },
+  {
+    id: 8,
+    nombre: 'Padre Rico, Padre Pobre -Robert Kiyosaki y Sharon Lechter',
+    descripcion: 'Padre rico, padre pobre es fácil de leer y sus mensajes más importantes...',
+    precio: 150000.00,
+    imagen: './assets/img/libro-4.png',
+    categoria: 'bebidas',
+    cantidad: 18
+  },
+  {
+    id: 9,
+    nombre: 'El mundo de Sofía, de Jostein Gaarder',
+    descripcion: 'Nuestra propia existencia contribuye a decidir cómo percibimos las cosas ...',
+    precio: 50000.00,
+    imagen: './assets/img/libro-1.png',
+    categoria: 'bebidas',
+    cantidad: 15
+  }
+
+]
+
+export default db
